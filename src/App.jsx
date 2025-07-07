3d2009ab5855ac5540eb2f5330efb7af4af4db54
Yuvika <kumaryuvika04@gmail.com>
2025-07-07 21:24:28 +0530

    feat:add detect location in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Products from "./pages/Products"
-import Home from "./pages/Home"
-import Contact from "./pages/Contact"
-import Cart from "./pages/Cart"
-import About from "./pages/About"
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [location, setLocation] = useState();
+  const [openDropdown,setOpenDropdown]=useState(false);
+
+  const getLocation = async () => {
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      const { latitude, longitude } = pos.coords;
+
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+      try {
+        const location = await axios.get(url);
+        const exactLocation = location.data.address;
+        setLocation(exactLocation);
+        setOpenDropdown(false);
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/products" element={<Products/>}></Route>
-      <Route path="/contact" element={<Contact/>}></Route>
-      <Route path="/cart" element={<Cart/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-    </Routes>
+      <Navbar location={location} getLocation={getLocation} openDropdown={openDropdown} setOpenDropdown={setOpenDropdown}/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,15 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import { MapPin } from "lucide-react";
+import { useState } from "react";
+import { CgClose } from "react-icons/cg";
 import { FaCaretDown } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { Link, NavLink } from "react-router-dom";
 
-const Navbar = () => {
-  const location = false;
+const Navbar = ({location,getLocation,openDropdown,setOpenDropdown}) => {
+
+const toggleDropdown=()=>{setOpenDropdown(open=>!open)}
+
   return (
     <div className="bg-white py-3 shadow-2xl">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
@@ -16,10 +20,21 @@ const Navbar = () => {
           <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
             <MapPin className="text-orange-500"></MapPin>
             <span className="font-semibold">
-              {location ? <div></div> : "Add Address"}
+              {location ? <div className="-space-y-2">
+                <p>{location.county}</p>
+                <p>{location.state}</p>
+              </div> : "Add Address"}
             </span>
-            <FaCaretDown />
+           <FaCaretDown onClick={toggleDropdown}/>
           </div>
+          {
+            openDropdown && (
+              <div className="w-[200px] h-max shadow-xl z-50 bg-white fixed top-16 left-60 p-2 rounded-md border-orange-500 border-1">
+               <h4 className=" mb-4 text-md flex justify-between">Change Location<span onClick={toggleDropdown}><CgClose className="cursor-pointer"/></span></h4>
+               <button onClick={getLocation} className="bg-orange-500 text-white px-3 py-1 cursor-pointer rounded-md">Detect Location</button>
+              </div>
+            )
+          }
         </div>
         <nav className="flex gap-7 items-center">
           <ul className="flex gap-7 items-center text-l font-semibold">
